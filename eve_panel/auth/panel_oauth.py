0c3cdd2302c0dbc207e08baad481ce5f4b1e3f6e
Yossi Mosbacher <joe.mosbacher>
2021-08-31 14:09:26 +0300

    avoid setting token to None

--- eve_panel/auth/panel_oauth.py
+++ eve_panel/auth/panel_oauth.py
@@ -3,6 +3,6 @@ from .bearer import EveBearerAuth
 class EvePanelAuth(EveBearerAuth):
     def login(self):
         import panel as pn
-        self.token = pn.state.access_token
+        self.token = pn.state.access_token or ""
         return bool(self.token)
 
