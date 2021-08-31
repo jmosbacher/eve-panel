6504a7e6dda6454acdecb7a73e8f2e92fe86b9a2
Yossi Mosbacher <joe.mosbacher>
2021-08-31 14:09:31 +0300

    Bump version: 0.3.20 → 0.3.21

--- eve_panel/__init__.py
+++ eve_panel/__init__.py
@@ -15,7 +15,7 @@ Todo:
 
 __author__ = """Yossi Mosbacher"""
 __email__ = 'joe.mosbacher@gmail.com'
-__version__ = '0.3.20'
+__version__ = '0.3.21'
 import panel as pn
 
 from .auth import EveAuthBase
